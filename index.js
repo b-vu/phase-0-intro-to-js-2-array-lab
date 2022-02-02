80ebff60caf03c6952643746e7417d000cfd0da6
Brian Vu <brianvu7@gmail.com>
2022-02-02 09:13:54 -0600

    commit tree e868d839e0f5117f75b8e07d066a9a3b67d40306

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = name => {
+    cats.push(name);
+}
+
+const destructivelyPrependCat = name => {
+    cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.splice(cats.length - 1);
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.splice(0, 1);
+}
+
+const appendCat = name => {
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+const prependCat = name => {
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+const removeLastCat = () => {
+    const newArray = cats.slice(0, cats.length - 1);
+    return newArray;
+}
+
+const removeFirstCat = () => {
+    const newArray = cats.slice(1);
+    return newArray;
+}
